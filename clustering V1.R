031a269ddfa74ca748ea84fcf6249db717ad736b
sunny2929 <sunnysondhi@live.com>
2016-08-26 23:41:09 -0400

    Add files via upload

---
+++ clustering V1.R
@@ -0,0 +1,35 @@
+
+data(wine, package="rattle")
+head(wine)
+
+
+df<- scale(wine[-1])
+head(df)
+
+
+wssplot <- function(data, nc=15, seed=1234){
+	              wss <- (nrow(data)-1)*sum(apply(data,2,var))
+               	      for (i in 2:nc){
+		        set.seed(seed)
+	                wss[i] <- sum(kmeans(data, centers=i)$withinss)}
+	                
+		      plot(1:nc, wss, type="b", xlab="Number of Clusters",
+	                        ylab="Within groups sum of squares")
+	   }
+
+wssplot(df)
+
+library(NbClust)
+set.seed(1234)
+nc <- NbClust(df, min.nc=2, max.nc=15, method="kmeans")
+barplot(table(nc$Best.n[1,]),
+	          xlab="Numer of Clusters", ylab="Number of Criteria",
+		            main="Number of Clusters Chosen by 26 Criteria")
+
+ fit.km <- kmeans(df, centers = 3, iter.max= 200 )
+  str(fit.km)
+
+table(fit.km$cluster, wine$Type)
+
+library(cluster)
+clusplot(pam(df, 3))
